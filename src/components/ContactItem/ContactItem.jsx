22ad868f5ab2bce9044018b1f41e3186260efc3e
1evanter <rita.oleksenkoo@gmail.com>
2023-09-29 01:12:33 +0300

    add selectFilteredContacts selector

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -2,16 +2,11 @@ import { Item } from './ContactItem.styled';
 import { Button } from 'components/ContactForm/ContactForm.styled';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteContact } from 'redux/operations';
-import { selectContacts, selectFilter } from 'redux/selectors';
+import { selectFilteredContacts } from 'redux/selectors';
 
 export const ContactItem = () => {
   const dispatch = useDispatch();
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectFilter);
-
-  const filteredContacts = contacts.filter(({ name }) =>
-    name.toLowerCase().includes(filter.toLowerCase())
-  );
+  const filteredContacts = useSelector(selectFilteredContacts);
 
   return filteredContacts.map(({ name, phone, id }) => {
     return (
