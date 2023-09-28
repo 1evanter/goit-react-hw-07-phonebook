46564947a070c27548bf28a4bde10f1fd0ada636
1evanter <rita.oleksenkoo@gmail.com>
2023-09-28 22:53:59 +0300

    add useEffect for fetch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,26 @@
-import { ContactForm } from "./ContactForm/ContactForm";
-import { GlobalStyle } from "components/GlobalStyle";
-import { ContactList } from "./ContactList/ContactList";
-import { Filter } from "./Filter/Filter";
+import { ContactForm } from './ContactForm/ContactForm';
+import { GlobalStyle } from 'components/GlobalStyle';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchContacts } from 'redux/operations';
 
 export const App = () => {
- return (
-      <div style={{margin: "30px"}}>
-        <h1>Phonebook</h1>
-        <ContactForm />
-        <h2 style={{marginBottom: "10px"}}>Contacts</h2>
-        <Filter />
-        <ContactList />
-        <GlobalStyle/>
-      </div>
-  )
-}
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <div style={{ margin: '30px' }}>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2 style={{ marginBottom: '10px' }}>Contacts</h2>
+      <Filter />
+      <ContactList />
+      <GlobalStyle />
+    </div>
+  );
+};
