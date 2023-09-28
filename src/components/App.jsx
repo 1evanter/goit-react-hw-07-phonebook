a1bfb35ed5c287daa331c112ab6d293f36a956b8
1evanter <rita.oleksenkoo@gmail.com>
2023-09-29 00:51:22 +0300

    remove redux-persist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,14 @@ import { GlobalStyle } from 'components/GlobalStyle';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchContacts } from 'redux/operations';
+import { selectIsLoading, selectError } from 'redux/selectors';
 
 export const App = () => {
   const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -19,6 +22,7 @@ export const App = () => {
       <ContactForm />
       <h2 style={{ marginBottom: '10px' }}>Contacts</h2>
       <Filter />
+      {isLoading && !error && <b>Request in progress...</b>}
       <ContactList />
       <GlobalStyle />
     </div>
