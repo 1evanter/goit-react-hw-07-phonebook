3fb79aeb7673cabd21c1e8a1900d563b70b77162
1evanter <rita.oleksenkoo@gmail.com>
2023-09-28 22:07:02 +0300

    add fetch contacts operation

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,10 +1,16 @@
-import axios from "axios";
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = 'https://6515c5e609e3260018c9202f.mockapi.io';
 
-export const fetchContacts = createAsyncThunk('contacts/fetchAll', async () => {
-    const response = axios.get('/contacts');
-    return response.data
-})
-
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  async (_, thunkAPI) => {
+    try {
+      const response = axios.get('/contacts');
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
